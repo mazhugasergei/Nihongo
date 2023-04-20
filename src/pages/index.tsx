796ac85ac663ef3b284d7015196dc86d42186d4e
Markus Wedler <markuswedler8@gmail.com>
2023-04-20 23:21:03 +1000

    Fix typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,7 @@ export default () => {
         <div className="question">
           <div className="furigana">私<span>わたし</span></div>
           <input type="text" className="l-2" maxLength={2} data-index={0} onChange={inputChange} />
-          <div className="furigana">松代樹<span>まつだいつき</span></div>
+          <div className="furigana">松田樹<span>まつだいつき</span></div>
           です。
         </div>
         <div className="question">
