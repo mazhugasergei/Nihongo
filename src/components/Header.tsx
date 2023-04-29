1f632b7e9128a3d232cb22bfdb6ae86e9a7d6719
Markus Wedler <markuswedler8@gmail.com>
2023-04-29 13:12:06 +1000

    Add keyboard accessebility for furigana toggler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 // react
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 // next
 import Link from 'next/link'
 // zustand
 import useSettings from "@/store/useSettings"
 
 export default () => {
+  const furiganaToggleButton = useRef<HTMLDivElement>(null)
+
   // furigana
   const furiganaState = useSettings(state => state.furigana)
   const furiganaToggle = useSettings(state => state.furiganaToggle)
@@ -15,10 +17,17 @@ export default () => {
     document.body.style.lineHeight = furiganaState ? "1.8" : "1.5"
   }, [furiganaState])
 
+  const hnadleFuriganaToggle = (e: any) => {
+    if(e.code === "Space"){
+      e.preventDefault()
+      furiganaToggle()
+    }
+  }
+
   return (
     <header>
-      <Link href="/">Home</Link>   
-      <button onClick={furiganaToggle} style={{ cursor: "pointer", userSelect: "none" }}><div className="furigana">あ<span>あ</span></div></button>
+      <Link href="/">Home</Link>
+      <div ref={furiganaToggleButton} onKeyDown={hnadleFuriganaToggle} tabIndex={0} style={{ cursor: "pointer", userSelect: "none" }}><div className="furigana">あ<span>あ</span></div></div>
     </header>
   )
 }
